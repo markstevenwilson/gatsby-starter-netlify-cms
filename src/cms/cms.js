8c597fa1b8b30a1ab61117069f72114876de02b0
Mark Wilson <markus.s.wilson@gmail.com>
2020-07-14 11:03:52 -0600

    Portfolio Content

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,13 +4,16 @@ import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
+import PortfolioItemPreview from './preview-templates/PortfolioItemPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
+
 
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
+CMS.registerPreviewTemplate('portfolio', PortfolioItemPreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)

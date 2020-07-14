7a42afe10ee748557de34bb6b8cf97f845271159
Mark Wilson <markus.s.wilson@gmail.com>
2020-07-14 13:10:47 -0600

    Config fixes for Portfolio Rendering

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -5,6 +5,7 @@ import { Link, graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Features from '../components/Features'
 import BlogRoll from '../components/BlogRoll'
+import PortfolioRoll from '../components/PortfolioRoll'
 
 export const IndexPageTemplate = ({
   image,

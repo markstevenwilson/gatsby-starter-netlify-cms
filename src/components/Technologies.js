875f04a9033c43dedd02e97950b60ea49494626b
Mark Wilson <markus.s.wilson@gmail.com>
2020-07-22 11:47:24 -0600

    tech

--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -1,24 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
 
 const Technologies = ({ data }) => (
-    <div className="columns is-multiline">
+    <div className="columns">
         {data.map((technology) => (
-            <div key={technology.techname} className="column is-3">
+            <div key={technology.techname} className="column">
                 <section className="section">
                     <h4 className="has-text-centered has-text-weight-semibold">
                         {technology.techname}
                     </h4>
                     <h2 className="is-size-1 has-text-weight-bold has-text-primary has-text-centered">
-                        <PreviewCompatibleImage imageInfo={technology.svgicon} />
+                        ${technology.svgicon}
                     </h2>
                     <p className="has-text-weight-semibold">{technology.description}</p>
+                    <ul>
                         {technology.items.map((item) => (
                             <li key={item} className="is-size-5">
                                 {item}
                             </li>
                         ))}
+                    </ul>
                 </section>
             </div>
         ))}
